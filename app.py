958121e10c7b23cdb25a1947215f8c2dcafc024c
Ayaka Oishi <ayakaoishi@Ayaka-no-MacBook-Air.local>
2019-08-28 18:55:36 -0400

    I made change

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 tabtitle = 'DC Housing'
 myheading='Analysis of housing prices in Washington DC'
-neighborhood='Brookland
+neighborhood='Brookland'
 color1='pink'
 color2='#1B03B1'
 sourceurl = 'https://www.kaggle.com/christophercorrea/dc-residential-properties/'
